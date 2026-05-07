6eaba86d543727f1bd2304cd23669efaaa44d65c
CodeSagePath <rishigarg185@gmail.com>
2026-05-08 02:21:49 +0530

    Updates:   - Add userscript distribution files   - Updated documentation

---
+++ wasabee.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id            Wasabee.user.js
+// @name          IITC plugin: Wasabee Draw Tools
+// @namespace     https://wasabee.rocks/
+// @version       0.1.20260507173954-hightower-fcc269f0
+// @updateURL     https://codesagepath.github.io/Wasabee-IITC/hightower/wasabee.meta.js
+// @downloadURL   https://codesagepath.github.io/Wasabee-IITC/hightower/wasabee.user.js
+// @description   Wasabee is not for dating.
+// @author        Wasabee Project Team
+// @match         https://intel.ingress.com/*
+// @category      Draw
+// @grant         none
+// ==/UserScript==
+
